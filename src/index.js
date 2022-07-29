a09769184bcd4e96ab40d2c200b5288c3084c406
Rigina <papakhina.rigina@yandex.ru>
2022-07-29 11:52:29 +0300

    react added

--- src/index.js
+++ src/index.js
@@ -1,54 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore} from 'redux';
+import reducer from './reducer';
+
+import { Provider } from 'react-redux';
+import App from './components/App';
 
 
-const initialState = {value: 0};
+const store = createStore(reducer);
 
-const reducer =(state=initialState, action)=>{
-  switch(action.type){
-    case 'INC':
-      return {
-        ...state,
-        value: state.value +1
-      };
-      case 'DEC':
-        return {
-          ...state,
-          value: state.value -1
-        };
-        case 'RND':
-          return {
-            ...state,
-            value: state.value * action.payload
-          };
-      default:
-        return state;
-    }
-  }
-
-const store = createStore(reducer)
-
-const update = ()=>{
-  document.getElementById('counter').textContent = store.getState().value;
-}
-
-store.subscribe(update);
-
-const inc =()=>({type:'INC'});
-const dec =()=>({type:'DEC'});
-const rnd =(value)=>({type:'RND', payload: value});
-
-document.getElementById('inc').addEventListener('click',()=>{
-  store.dispatch(inc());
-});
-document.getElementById('dec').addEventListener('click',()=>{
-  store.dispatch(dec());
-});
-document.getElementById('rnd').addEventListener('click',()=>{
-  const value = Math.floor(Math.random()*10);
-  store.dispatch(rnd(value));
-});
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+     <Provider store={store}>
+     <App/>
+     </Provider>
+    </React.StrictMode>
+  );
 
 
 
@@ -56,17 +24,5 @@ document.getElementById('rnd').addEventListener('click',()=>{
 
 
 
-// let state = reducer(initialState, {type: 'INC'});
-// state = reducer(state,{type: 'INC'});
-// state = reducer(state,{type: 'INC'});
-// state = reducer(state,{type: 'INC'});
-// console.log(state)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
-);
 
 
